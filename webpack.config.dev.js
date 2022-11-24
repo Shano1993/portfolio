b5ea0e26177abdb915f4dce0015f21e4e4e709c4
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-24 10:19:23 +0100

    skeleton

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+
+const development = {
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/i,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            import: true,
+                            sourceMap: true,
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'image/[name][ext]',
+                }
+            },
+        ]
+    },
+}
+
+module.exports = development;
